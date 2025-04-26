2e5dc9b2347f8bf600e1c8005cc2743d4f788eb9
Dmitry Conquer <dmitry.conquer@gmail.com>
2025-04-26 13:52:48 +0300

    feat: add smooth scrolling and back-to-top button functionality; update dependencies and styles

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,7 +1,15 @@
-import "../styles/main.scss";
-
 import Sliders from "./components/sliders";
+import Scroll from "./scroll";
+import BackTopButton from "./components/back-top-button";
+import "../styles/main.scss";
 
 document.addEventListener("DOMContentLoaded", () => {
   new Sliders();
+
+  const scroll = new Scroll();
+  scroll.initSmoothScroll();
+  scroll.initAOS();
+
+  const backTopButton = new BackTopButton();
+  backTopButton.init();
 });
